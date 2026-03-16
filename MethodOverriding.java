ba2736714ec2b6092f2157f2409bb4661846cef0
Nandakishore630 <164779830+Nandakishore630@users.noreply.github.com>
2026-03-16 10:47:55 +0530

    Delete MethodOverriding.java

--- MethodOverriding.java
+++
@@ -1,9 +0,0 @@
-public class CharFrequency{
- public static void main(String[] args){
-  String s="banana";
-  int count=0;
-  for(char c:s.toCharArray())
-   if(c=='a') count++;
-  System.out.println(count);
- }
-}

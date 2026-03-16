350e80c29c61629db01bcc9692d622a6da904053
Nandakishore630 <164779830+Nandakishore630@users.noreply.github.com>
2026-03-16 10:42:01 +0530

    Delete DataTypesDemo.java

--- DataTypesDemo.java
+++
@@ -1,8 +0,0 @@
-class Shape{ void draw(){System.out.println("Shape");} }
-class Circle extends Shape{ void draw(){System.out.println("Circle");} }
-public class MethodOverriding{
- public static void main(String[] args){
-  Shape s=new Circle();
-  s.draw();
- }
-}

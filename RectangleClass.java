b765b9d4abe651abe77fb8778d425dfd3fed8730
Nandakishore630 <164779830+Nandakishore630@users.noreply.github.com>
2026-03-16 10:48:07 +0530

    Delete RectangleClass.java

--- RectangleClass.java
+++
@@ -1,8 +0,0 @@
-public class LargestInArray{
- public static void main(String[] args){
-  int[] arr={3,7,2,9,5};
-  int max=arr[0];
-  for(int i:arr) if(i>max) max=i;
-  System.out.println(max);
- }
-}

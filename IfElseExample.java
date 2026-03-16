40d29d661bd29dc514811f44b29e3c4deb61e8b4
Nandakishore630 <164779830+Nandakishore630@users.noreply.github.com>
2026-03-16 10:46:48 +0530

    Delete IfElseExample.java

--- IfElseExample.java
+++
@@ -1,9 +0,0 @@
-public class DataTypesDemo{
- public static void main(String[] args){
-  int a=10;
-  double b=5.5;
-  char c='A';
-  boolean d=true;
-  System.out.println(a+" "+b+" "+c+" "+d);
- }
-}
